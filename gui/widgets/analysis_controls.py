a84797d2deeefabc203f60f7aa6df57c676a2278
abraker95 <abraker95@gmail.com>
2019-04-02 20:08:53 -0400

    Make the analysis controls graphs dockable

--- gui/widgets/analysis_controls.py
+++ gui/widgets/analysis_controls.py
@@ -2,8 +2,11 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
+from pyqtgraph.dockarea import *
+
 from gui.objects.graph.graphs.tapping_intervals_graph import TappingIntervalsGraph
 from gui.objects.graph.graphs.velocity_graph import VelocityGraph
+from gui.objects.graph.graphs.rhythmic_complexity_graph import RhythmicComplexityGraph
 
 
 
@@ -18,8 +21,9 @@ class AnalysisControls(QWidget):
 
 
     def init_gui_elements(self):
-        self.layout = QVBoxLayout()
-        self.label  = QLabel('Analysis Controls', self)
+        self.layout    = QVBoxLayout()
+        self.dock_area = DockArea()
+        self.label     = QLabel('Analysis Controls', self)
         
         # TODO: List of dockable widgets
         self.graphs = [ 
@@ -31,8 +35,14 @@ class AnalysisControls(QWidget):
     def construct_gui(self):
         self.setLayout(self.layout)
 
+        # TODO: Handle closing of floating docks
         for graph in self.graphs:
-            self.layout.addWidget(graph)
+            dock = Dock(graph.getPlotItem().titleLabel.text, size=(1, 1))    # TODO: , closable=True
+            dock.addWidget(graph)
+            self.dock_area.addDock(dock, 'bottom')
+
+        self.layout.addWidget(self.dock_area)
+        self.layout.addWidget(self.label)
 
 
     def update_gui(self):
