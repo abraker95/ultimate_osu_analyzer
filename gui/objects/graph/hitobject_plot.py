a26b9b5a80481aaec694af593dd094ba1f800cb9
abraker95 <abraker95@gmail.com>
2019-04-05 18:51:49 -0400

    Save data in the plot, and allow setting y-pos of the hitobject plot; to allow response graphing

--- gui/objects/graph/hitobject_plot.py
+++ gui/objects/graph/hitobject_plot.py
@@ -14,9 +14,14 @@ class HitobjectPlot(pyqtgraph.GraphItem):
         self.pen = pyqtgraph.mkPen(width=HitobjectPlot.HITOBJECT_RADIUS)
         self.pen.setCapStyle(QtCore.Qt.RoundCap)
         self.setPen(self.pen)
+
+        self.data = None
     
 
-    def update_data(self, data):
+    def update_data(self, data, y_pos=0):
+        if data == None: return
+        self.data = data
+
         pos  = []
         adj  = []
         size = []
@@ -24,19 +29,19 @@ class HitobjectPlot(pyqtgraph.GraphItem):
         obj_num = -1
 
         for (time_start, time_end) in data:
-            pos.append([ time_start, 0 ])
+            pos.append([ time_start, y_pos ])
             size.append(HitobjectPlot.HITOBJECT_RADIUS)
             obj_num += 1
 
             # Slider end
             if time_start != time_end:
-                pos.append([ time_end, 0 ])
+                pos.append([ time_end, y_pos ])
                 size.append(0)
                 obj_num += 1
 
                 adj.append([ obj_num - 1, obj_num ])
 
-        pos = np.array(pos, dtype=np.int)
+        pos = np.array(pos, dtype=np.float)
         adj = np.array(adj, dtype=np.int)
 
         self.setData(pos=pos, adj=adj, size=size, symbol='o', pxMode=True)
