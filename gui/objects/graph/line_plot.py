58e9696250e0aa9cf69983c2ef76b7460fd75a35
abraker95 <abraker95@gmail.com>
2019-04-05 18:50:59 -0400

    Add function to line plot to allow setting of parameters individually

--- gui/objects/graph/line_plot.py
+++ gui/objects/graph/line_plot.py
@@ -13,3 +13,7 @@ class LinePlot(pyqtgraph.PlotCurveItem):
     def update_data(self, data):
         data_x, data_y = data
         self.setData(x=data_x, y=data_y)
+
+    
+    def update_xy(self, data_x, data_y):
+        self.setData(x=data_x, y=data_y)
