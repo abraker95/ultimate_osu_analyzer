37bef614be8c7a00ee3d1101e0f3b4f7414f9d83
abraker95 <abraker95@gmail.com>
2019-04-13 17:59:27 -0400

    Add ability to collect returned values from callback functions; comments

--- misc/callback.py
+++ misc/callback.py
@@ -3,12 +3,15 @@
 # because qt is retarded and doesn't support multiple inheritence
 #
 # Basically this is used as a decorator and adds a list of functions to the function.
-# When emit is called, all the functions in the list are run
+# When emit is called, all the functions in the list are run. Their returns are collected
+#     into Func.returns as the function execute
 def callback(func):
+
     def Func(*args, **kwargs):
         return func(*args, **kwargs)
     
     class FuncHelper():
+
         @staticmethod
         def add_callback(callback):
             Func.callbacks.add(callback)
@@ -22,20 +25,32 @@ def callback(func):
 
         @staticmethod
         def exec_callbacks(*args, **kwargs):
+            Func.returns = {}
+            # Copy because one of the callbacks can be a disconnect to the refered callbacks
+            # This would effectively change the size of Func.callbacks, causing an exception
             for callback in Func.callbacks.copy():
                 if not Func.anti_recurse:
                     Func.anti_recurse = True
-                    callback(*args, **kwargs)
+                    Func.returns[callback] = callback(*args, **kwargs)
                     Func.anti_recurse = False
 
             return Func
 
-    # This is required or emiting from a function it is connected to will result in infinite recursion
+        @staticmethod 
+        def get_return_for(callback):
+            if not callback in Func.returns: return None
+            else: return Func.returns[callback]
+
+    # This is required to ensure emiting from a function it is connected to
+    # will not result in infinite recursion via self reference. This causes
+    # the connected function to only execute one time from per specific function's emit
     Func.anti_recurse = False
-    
+    Func.returns      = {}
+
     Func.connect      = FuncHelper.add_callback
     Func.disconnect   = FuncHelper.rmv_callback
     Func.emit         = FuncHelper.exec_callbacks
+    Func.ret          = FuncHelper.get_return_for
     
     Func.callbacks = set()
     return Func
