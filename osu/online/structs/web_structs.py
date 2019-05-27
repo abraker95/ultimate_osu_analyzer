05c3fef80e6bf2df2ac91a811afd162d4727e79d
abraker <abraker95@gmail.com>
2019-05-27 15:41:07 -0400

    Cache the fetched data

--- osu/online/structs/web_structs.py
+++ osu/online/structs/web_structs.py
@@ -10,11 +10,14 @@ class WebBeatmapset(JsonObj):
 
     def __init__(self, data):
         JsonObj.__init__(self, data)
-        self.name = self.artist + ' - ' + self.title + ' (' + self.creator + ') '
+        self.name         = self.artist + ' - ' + self.title + ' (' + self.creator + ') '
+        self.web_beatmaps = None
 
 
-    def get_beatmaps(self):
-        return [ WebBeatmap(self.name, beatmap) for beatmap in self.beatmaps ]
+    def get_beatmaps(self, refresh=False):
+        if (self.web_beatmaps == None) or refresh:
+            self.web_beatmaps = [ WebBeatmap(self.name, beatmap) for beatmap in self.beatmaps ]
+        return self.web_beatmaps
         
 
 
@@ -22,11 +25,14 @@ class WebBeatmap(JsonObj):
     
     def __init__(self, name, data):
         JsonObj.__init__(self, data)
-        self.name = name + '[' + self.version + ']'
+        self.name       = name + '[' + self.version + ']'
+        self.web_scores = None
 
 
-    def get_scores(self):
-        return [ WebScore(self.name, score) for score in OsuOnline.fetch_scores(self.id, self.mode) ]
+    def get_scores(self, refresh=False):
+        if (self.web_scores == None) or refresh:
+            self.web_scores = [ WebScore(self.name, score) for score in OsuOnline.fetch_scores(self.id, self.mode) ]
+        return self.web_scores
 
 
     def download_beatmap(self, filepath):
@@ -47,23 +53,29 @@ class WebScore(JsonObj):
         self.name = name + ' ~ ' + self.user['username']
         if len(self.mods) > 0:  
             self.name += ' +' + ''.join(self.mods)
-
-
-    def download_replay_api(self, filepath):
-        replay_data = OsuApi.fetch_replay_file(self.id, self.user['username'], self.mode, self.mods)
-        pathname    = filepath + '/' + self.name + '.osr'
-        ReplayIO.save_replay(replay_data, pathname)
+        
+        self.replay_data = None
 
 
     def get_replay_data_api(self):
-        return OsuApi.fetch_replay_file(self.id, self.user['username'], self.mode, self.mods)
+        if self.replay_data == None:
+            self.replay_data = OsuApi.fetch_replay_file(self.id, self.user['username'], self.mode, self.mods)
+        return self.replay_data
+        
 
-
-    def download_replay_web(self, filepath):
-        replay_data = OsuApi.fetch_replay_file(self.beatmap['id'], self.user['username'], self.mode, self.mods)
+    def download_replay_api(self, filepath):
+        replay_data = self.get_replay_data_api()
         pathname    = filepath + '/' + self.name + '.osr'
         ReplayIO.save_replay(replay_data, pathname)
 
 
     def get_replay_data_web(self):
-        return OsuOnline.fetch_replay_file(self.id)
+        if self.replay_data == None:
+            self.replay_data = OsuOnline.fetch_replay_file(self.id)
+        return self.replay_data
+
+
+    def download_replay_web(self, filepath):
+        replay_data = self.get_replay_data_web()
+        pathname    = filepath + '/' + self.name + '.osr'
+        ReplayIO.save_replay(replay_data, pathname)
