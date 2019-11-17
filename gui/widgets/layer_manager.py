2b4ad398e3f90b0888db203d24909a12d9c495d3
abraker <abraker95@gmail.com>
2019-11-17 18:26:54 -0500

    Add ability to remove layers; Fix a few things about group hierarchy data (but still has bugs)

--- gui/widgets/layer_manager.py
+++ gui/widgets/layer_manager.py
@@ -11,8 +11,9 @@ from misc.callback import callback
 
 class LayerManagerItem(QTreeWidgetItem):
 
-    def __init__(self, parent, data):
+    def __init__(self, parent, path, data):
         self.layer = data
+        self.path  = path
         columns = (self.layer.name, )
 
         QTreeWidgetItem.__init__(self, parent, columns)
@@ -26,11 +27,13 @@ class LayerManagerItem(QTreeWidgetItem):
         self.layer.layer_changed()
 
 
+
 class LayerManagerGroupItem(QTreeWidgetItem):
 
-    def __init__(self, parent, name):
+    def __init__(self, parent, path, name):
         QTreeWidgetItem.__init__(self, parent, [ name ])
         self.setCheckState(0, Qt.Checked)  # Set checked on 0th column
+        self.path = path
 
 
     def layer_enable_event(self):
@@ -59,15 +62,23 @@ class LayerManager(QWidget):
         self.hierarchy = { '.' : [ self.layer_list, {} ] }
         '''
         { 
-            '.' : [ self.layer_list, 
-            {
-                'groupa' : [
-
-                ],
-                'groupb' : [
-
-                ]
-            }
+            '.' : [
+                self.layer_list, 
+                {
+                    <subgroup> : [
+                        LayerManagerGroupItem,
+                        <subgroup> : [
+                            ...
+                        ]
+                    ],
+                    <subgroup> : [
+                        LayerManagerGroupItem,
+                        <subgroup> : [
+                            ...
+                        ]
+                    ]
+                }
+            ]
         }
         '''
 
@@ -89,42 +100,83 @@ class LayerManager(QWidget):
 
 
     def add_layer(self, path, layer):
-        group_names = path.split('.')    
-        group       = self.__set_groups(group_names)
+        group = self.__set_groups(path + ',' + layer.name)
+        group[1][layer.name] = [ LayerManagerItem(group[0], path, layer), {} ]
 
-        LayerManagerItem(group, layer)
         self.scene.add_layer(layer)
 
-
-    def rmv_layer(self, layer):
-        self.scene.rmv_layer(layer)
+        print(self.hierarchy)
 
 
     def get_scene(self):
         return self.scene
 
 
-    def __set_groups(self, group_names):
-        curr_group = self.hierarchy['.']
+    def __set_groups(self, path):
+        path, file_name = path.split(',')
+        group_names = path.split('.')
+        curr_group  = self.hierarchy['.']
 
         for group_name in group_names:
             if not group_name in curr_group[1]:
-                self.__set_group(curr_group, group_name)
+                self.__set_group(curr_group, path, group_name)
             curr_group = curr_group[1][group_name]
 
-        return curr_group[0]
+        return curr_group
 
 
-    def __set_group(self, group, group_name):
-        group[1][group_name] = [ LayerManagerGroupItem(group[0], group_name), {} ]
+    def __set_group(self, group, path, group_name):
+        group[1][group_name] = [ LayerManagerGroupItem(group[0], path, group_name), {} ]
+
+
+    def __get_group_parent(self, path):
+        group_names = path.split('.')
+        curr_group  = self.hierarchy['.']
+
+        for group_name in group_names[:-1]:
+            curr_group = curr_group[1][group_name]
+
+        return curr_group
 
 
     def __check_item_status(self, item):
         item.layer_enable_event()
 
 
+    def __remove(self, item):
+        if type(item) == type(None): 
+            return
+
+        if type(item) == LayerManagerItem:
+            self.scene.rmv_layer(item.layer)
+            item.parent().removeChild(item)
+
+        elif type(item) == LayerManagerGroupItem:
+            for _ in range(item.childCount()):
+                self.__remove(item.child(0))
+
+            parent = self.__get_group_parent(item.path)[0]
+            if type(parent) == QTreeWidget:
+                parent.takeTopLevelItem(parent.indexOfTopLevelItem(item))
+            elif type(parent) == LayerManagerGroupItem:
+                parent.removeChild(item)
+            else:
+                print('[ ERROR ] Unexpected layer parent type', type(parent))
+
+            self.__get_group_parent(item.path)[1] = {}
+
+        else:
+            print('[ ERROR ] item type', type(item))
+            
+
     def __right_click_menu(self, pos):
         item = self.layer_list.itemAt(pos)
         if item == None: return
 
-        # TODO
+        remove_action = QAction('&Remove')
+        remove_action.setStatusTip('Removes layer or group')
+        remove_action.triggered.connect(lambda _, item=item: self.__remove(item))
+
+        menu = QMenu(self)
+        menu.addAction(remove_action)
+        menu.exec(self.mapToGlobal(pos))
