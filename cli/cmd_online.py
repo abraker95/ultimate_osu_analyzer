158ccdfa643af1c0074839ec0f5230b1f8b917f7
abraker <abraker95@gmail.com>
2020-02-29 14:05:41 -0500

    No longer need name to get scores; Add score into fetching via api in CmdOnline

--- cli/cmd_online.py
+++ cli/cmd_online.py
@@ -12,8 +12,19 @@ class CmdOnline():
 
 
     @staticmethod
-    def get_scores(beatmap_id, mode, name):
+    def get_beatmap(beatmap_id, mode):
+        return OsuOnline.fetch_beatmap_file(beatmap_id, ret_name=True)
+
+
+    @staticmethod
+    def get_scores(beatmap_id, mode, name=None):
+        if name == None: name = OsuOnline.fetch_beatmap_name(beatmap_id)
         return [ WebScore(name, score) for score in OsuOnline.fetch_scores(beatmap_id, mode) ]
+
+
+    @staticmethod
+    def get_scores_api(beatmap_id, mode, mods, name=None):
+        return OsuApi.fetch_score_info(beatmap_id, gamemode=mode, mods=mods)
 
 
     @staticmethod
