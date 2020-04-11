8c926e7fb751390eeed639430ea0cdce734cce83
abraker <abraker95@gmail.com>
2020-04-11 18:21:38 -0400

    Add functions that calculate mania key press rates

--- analysis/osu/mania/map_metrics.py
+++ analysis/osu/mania/map_metrics.py
@@ -5,6 +5,7 @@ from misc.numpy_utils import NumpyUtils
 
 from osu.local.beatmap.beatmap import Beatmap
 from analysis.osu.mania.map_data import ManiaMapData
+from analysis.osu.mania.action_data import ManiaActionData
 
 
 
@@ -14,9 +15,9 @@ class ManiaMapMetrics():
     Raw metrics
     """
     @staticmethod
-    def calc_action_rate(action_data, window_ms=1000):
+    def calc_press_rate(action_data, window_ms=1000):
         """
-        Calculates number of actions there are across all columns within indicated ``window_ms`` of time
+        Calculates presses per second across all columns within indicated ``window_ms`` of time
 
         Parameters
         ----------
@@ -33,11 +34,81 @@ class ManiaMapMetrics():
             ``aps`` are actions per second at indicated time.
         """
         aps = []
-        for start in range(len(action_data)):
-            try: aps.append([ action_data[start, 0],  np.where(action_data[start, 0] + window_ms <= action_data[:, 0])[0][0] - start ])
-            except IndexError: break
+        for i in range(len(action_data)):
+            actions_in_range = ManiaActionData.get_actions_between(action_data, action_data[i, 0] - window_ms, action_data[i, 0])
+            num_actions = ManiaActionData.count_actions(actions_in_range, ManiaActionData.PRESS)
+            aps.append([ action_data[i, 0], 1000*num_actions/window_ms ])
 
         return np.asarray(aps)
+
+
+    @staticmethod
+    def calc_press_rate_col(action_data, col, window_ms=1000):
+        """
+        Calculates presses per second in the specified column within indicated ``window_ms`` of time
+
+        Parameters
+        ----------
+        action_data : numpy.array
+            Action data from ``ManiaMapData.get_action_data``
+
+        col : int
+            Column to calculated presses per second for
+
+        window_ms : int
+            Duration in milliseconds for which actions are counted up
+
+        Returns
+        -------
+        (numpy.array, numpy.array)
+        Tuple of ``(times, aps)``. ``times`` are timings corresponding to recorded actions per second. 
+            ``aps`` are actions per second at indicated time.
+        """
+        action_data = action_data[:, [0, col]]
+        aps = []
+
+        for i in range(len(action_data)):
+            actions_in_range = ManiaActionData.get_actions_between(action_data, action_data[i, 0] - window_ms, action_data[i, 0])
+            num_actions = ManiaActionData.count_actions(actions_in_range, ManiaActionData.PRESS)
+            aps.append([ action_data[i, 0], 1000*num_actions/window_ms ])
+
+        return np.asarray(aps)
+
+
+    @staticmethod
+    def calc_max_press_rate_per_col(action_data, window_ms=1000):
+        """
+        Takes which column has max presses per second within indicated ``window_ms`` of time
+
+        Parameters
+        ----------
+        action_data : numpy.array
+            Action data from ``ManiaMapData.get_action_data``
+
+        window_ms : int
+            Duration in milliseconds for which actions are counted up
+
+        Returns
+        -------
+        (numpy.array, numpy.array)
+        Tuple of ``(times, max_aps_per_col)``. ``times`` are timings corresponding to recorded actions per second. 
+            ``max_aps_per_col`` are max actions per second at indicated time.
+        """
+        keys = ManiaActionData.num_keys(action_data)
+        max_aps_per_col = []
+
+        for i in range(len(action_data)):
+            aps_col = []
+            for col in range(1, keys + 1):
+                col_data = action_data[:, [0, col]]
+
+                actions_in_range = ManiaActionData.get_actions_between(col_data, col_data[i, 0] - window_ms, col_data[i, 0])
+                num_actions = ManiaActionData.count_actions(actions_in_range, ManiaActionData.PRESS)
+                aps_col.append(1000*num_actions/window_ms)
+            
+            max_aps_per_col.append([ action_data[i, 0], max(aps_col) ])
+
+        return np.asarray(max_aps_per_col)
 
 
     @staticmethod
@@ -48,7 +119,7 @@ class ManiaMapMetrics():
         Parameters
         ----------
         action_data : numpy.array
-            Action data from ``ManiaMapData.get_action_data``
+            Action data from ``ManiaActionData.get_action_data``
 
         Returns
         -------
