72755038b1b199edbbe2c473af81b8d6c6d61c97
abraker <abraker95@gmail.com>
2020-04-19 14:22:10 -0400

    Add support for pen_width in mania action data layers

--- analysis/osu/mania/mania_layers.py
+++ analysis/osu/mania/mania_layers.py
@@ -53,16 +53,16 @@ class ManiaLayers():
     '''
 
     @staticmethod
-    def ManiaActionLayer(painter, columns, time, spatial_data, action_data):
+    def ManiaActionLayer(painter, columns, time, spatial_data, action_data, pen_width=1):
         start_time, end_time = spatial_data[0], spatial_data[1]
-
         action_data = action_data[np.logical_and(start_time <= action_data[:,0], action_data[:,0] <= end_time)]
+
         for data in action_data:
             for col in range(1, len(data)):
                 if data[col] == ManiaActionData.FREE:    continue
-                if data[col] == ManiaActionData.PRESS:   painter.setPen(QColor(0,   150, 0,   255))
-                if data[col] == ManiaActionData.HOLD:    painter.setPen(QColor(255,   0, 255, 255))
-                if data[col] == ManiaActionData.RELEASE: painter.setPen(QColor(255, 255, 0,   255))
+                if data[col] == ManiaActionData.PRESS:   painter.setPen(QPen(QColor(0,   150, 0,   255), pen_width))
+                if data[col] == ManiaActionData.HOLD:    painter.setPen(QPen(QColor(255,   0, 255, 255), pen_width))
+                if data[col] == ManiaActionData.RELEASE: painter.setPen(QPen(QColor(255, 255, 0,   255), pen_width))
 
                 pos_x, pos_y, scaled_note_width = get_draw_data(*spatial_data[2:], col - 1, time, data[0])
                 painter.drawLine(pos_x, pos_y, pos_x + scaled_note_width, pos_y)
