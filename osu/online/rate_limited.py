7a370211f3315ea429022e0e9cec76ca8fd5daab
abraker <abraker95@gmail.com>
2020-08-23 12:06:43 -0400

    Fix time.clock being deprecated since python 3.3

--- osu/online/rate_limited.py
+++ osu/online/rate_limited.py
@@ -5,7 +5,7 @@ def rate_limited(rate_limit=0, throw_exception=False):
 
     def wrap(func):
         def Func(*args, **kwargs):
-            try:    time_passed = time.clock() - Func.last_run_time
+            try:    time_passed = time.process_time() - Func.last_run_time
             except: time_passed = rate_limit + 1
             
             if time_passed < rate_limit:
@@ -13,7 +13,7 @@ def rate_limited(rate_limit=0, throw_exception=False):
                     raise Exception('This function is rate limited. Please wait ' + str(round(rate_limit - time_passed, 2)) + ' more seconds')
                 else:
                     time.sleep(rate_limit - time_passed)
-            Func.last_run_time = time.clock()
+            Func.last_run_time = time.process_time()
 
             return func(*args, **kwargs)
 
