54a6e4ce1615d3f099a8cc4913c7f9d2875a67f6
abraker <abraker95@gmail.com>
2020-09-10 16:37:26 -0400

    Remove unused (this is no longer planned)

--- gui/widgets/graph_manager.py
+++ gui/widgets/graph_manager.py
@@ -28,21 +28,6 @@ class GraphManager(QWidget):
         self.graphs = {}
 
 
-        '''
-        # TODO: 
-            A scripting text field that allows to create new metrics to graph
-            Default info avaliable:
-                hitobjects ->
-                    time
-                    pos
-
-            Ex:
-                # Interval metrics
-                x = [ hitobjects[i].time for i in range(1, len(hitobjects)) ]
-                y = [ hitobjects[i].time - hitobjects[i - 1].time in range(1, len(hitobjects)) ]
-        '''
-
-
     def construct_gui(self):
         self.setLayout(self.layout)
         self.layout.addWidget(self.dock_area)
@@ -78,27 +63,7 @@ class GraphManager(QWidget):
         self.dock_area.addDockWidget(Qt.LeftDockWidgetArea, dock)           # I have no idea why Left/Right is reversed
         self.graphs[graph.getPlotItem().titleLabel.text] = [ graph, dock ]
 
-    
-    '''
-    def add_graph_for_metric(self, metric, plot_type=None):
-        number_dimensions = metric.get_metric_dimensions()
 
-        # Validate and/or choose plot type
-        if plot_type != None:
-            if not plot_type.accepts_dim(number_dimensions):
-                raise Exception('Plot type indicated cannot accept ' + str(number_dimensions) + ' dimensions')
-        else:
-            if    number_dimensions == 1: return  # TODO
-            elif  number_dimensions == 2: plot_type = LinePlot()
-            elif  number_dimensions == 3: return  # TODO
-            elif  number_dimensions == 4: return  # TODO
-            else: return  # TODO: Unsupported?
-
-        # TODO: Support for other types of graphs that are not time related
-        self.add_graph(TemporalHitobjectGraph(plot_type, metric.name, metric))
-    '''
-
-            
     def remove_graph(self, graph_name):
         self.dock_area.removeDockWidget(self.graphs[graph_name][1])
         del self.graphs[graph_name]
